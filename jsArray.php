cbee0e89364f8d1bb4ce36cd05a0a254fce2f34b
Unknown <jasv61@gmail.com>
2017-10-31 15:44:32 +0100

    ajout du onkeypress sur le bouton ajouter un fruit

--- jsArray.php
+++ jsArray.php
@@ -1,48 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>JS ARRAYS</title>
-</head>
-<body>
-
-	<input type="text" id="newFruit">
-	<input type="button" value="Ajouter un fruit dans la liste" onclick="ajouter()">
-
-	<ul id="fruitList" >
-	</ul>
-	<input type="button" value="Debug" onclick="debug()">
-	<script>
-		var fruits = [];
-		
-		function ajouter () {
-			var nomFruit =document.getElementById('newFruit').value;
-			var newLength = fruits.push(nomFruit);
-			show();
-			document.getElementById("newFruit").value = "";
+	<head>
+		<title>JS ARRAYS</title>
+	</head>
+	<body>
+		<input type="text" id="newFruit" onkeypress="keyPressOnNewFruit()">
+		<input type="button" value="Ajouter un fruit dans la liste" onclick="ajouter()">
+		<ul id="fruitList" >
+		</ul>
+		<input type="button" value="Debug" onclick="debug()">
+		<script>
+			var fruits = [];
 			
-			
-		}
-
-		function show(){
-			document.getElementById("fruitList").innerHTML= "";
-			for (var i = 0; i < fruits.length; i++) {
-				document.getElementById("fruitList").innerHTML += "<li>"+fruits[i]+" <input type='button' value='Supprimer' onclick='supprimer("+i+")'></li>";
+			function ajouter () {
+				var nomFruit =document.getElementById('newFruit').value;
+				var newLength = fruits.push(nomFruit);
+				show();
+				document.getElementById("newFruit").value = "";
+				
+				
+			}
+			function show(){
+				document.getElementById("fruitList").innerHTML= "";
+				for (var i = 0; i < fruits.length; i++) {
+					document.getElementById("fruitList").innerHTML += "<li>"+fruits[i]+" <input type='button' value='Supprimer' onclick='supprimer("+i+")'></li>";
+				}
+			}
+			function supprimer(pos) {
+				var removedItem = fruits.splice(pos, 1);
+				show();
+			}
+			function debug () {
+				fruits.forEach(function(item, index, array) {
+				console.log(item, index);});
 			}
 
-		}
-
-		function supprimer(pos) {
-			var removedItem = fruits.splice(pos, 1);
-
-			show();
-		}
-
-		function debug () {
-			fruits.forEach(function(item, index, array) {
-  			console.log(item, index);});
-		}
-
-	</script>
-
-</body>
+			function keyPressOnNewFruit(){
+				if (event.keyCode == 13) {
+					ajouter();
+				}
+			}
+		</script>
+	</body>
 </html>
