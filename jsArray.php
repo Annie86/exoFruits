c9434456172bba6dc87d64a204277748ad60f75f
Unknown <jasv61@gmail.com>
2017-10-31 12:24:17 +0100

    Ajout nouveau bouton (supprimer) et modif function

--- jsArray.php
+++ jsArray.php
@@ -6,18 +6,25 @@
 <body>
 
 	<input type="text" id="newFruit">
-	<input type="button" value="Ajouter un fruit dans la liste">
+	<input type="button" value="Ajouter un fruit dans la liste" onclick="ajouter()">
 
 	<ul id="fruitList" >
-		<li>Orange <button>supprimer</button></li>
-
 	</ul>
+	s
 	<input type="button" value="Debug" onclick="debug()">
 	<script>
 		var fruits = [];
+		var button = "<input type='button' value='Supprimer' onclick='supprimer()'>";
+
 		function ajouter () {
-			var nomFruit =document.getElementById(newFruit).value;
+			var nomFruit =document.getElementById('newFruit').value;
 			var newLength = fruits.push(nomFruit);
+			document.getElementById("fruitList").innerHTML += "<li>"+fruits[newLength-1]+" "+button+"</li>";
+		}
+
+		function supprimer() {
+			var newLength = fruits.push(nomFruit);
+			var removedItem = fruits.splice(newLength-1);
 		}
 
 		function debug () {
